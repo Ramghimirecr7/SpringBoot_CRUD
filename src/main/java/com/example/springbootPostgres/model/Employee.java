688507709860a27d8f847a65affa6c56691d98f7
Ram Ghimire <ramghimire@Rams-MacBook-Pro.local>
2022-07-13 23:20:15 -0500

    added lombok

--- src/main/java/com/example/springbootPostgres/model/Employee.java
+++ src/main/java/com/example/springbootPostgres/model/Employee.java
@@ -1,7 +1,12 @@
 package com.example.springbootPostgres.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -27,38 +32,6 @@ public class Employee {
 
     @Column(name = "email",nullable = false)
     private String email;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Override
     public String toString() {
